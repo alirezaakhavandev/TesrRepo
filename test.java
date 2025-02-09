e291e6f7b7dee416e84063ab36a07e5f120f6c37
Alireza <alireza.akhavan.work@gmail.com>
2025-02-09 19:46:45 +0330

    Update test.java

--- test.java
+++ test.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 public class file{
     public static void main(String[] args) {
-        List<Students> students = List.of(
-            new Students(22, "Alex" ),
-            new Students(45, "John Wick")
-        );
+        
+    }
+
+    public String greeting(){
+        System.out.println("This is just a test")
     }
 }
 
-record Students(int id , String name ){
 
-} 
